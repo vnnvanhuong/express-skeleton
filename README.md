9abf4cdc17ca2cf84e1420f52a922a8c8aa295d3
Huong Nguyen <huong.nguyen@ubitec.ch>
2019-08-13 22:01:22 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# EXPRESS SKELETON
+
+> Description here
+
+## Features
+
+> Description here
+
+
+## Quickstart (Development)
+
+```bash
+# install dependencies:
+npm install
+
+# run the app
+npm run start:dev
+```
+
+## Deployment (Production)
+
+> Description here
---
+++ app.js
@@ -0,0 +1,35 @@
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  err.status = err.status || 500;
+  err.message = err.message || 'Internal Server Error';
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  console.log(err);
+
+  res.status(err.status);
+  res.render({ errorMessage: err.message });
+});
+
+module.exports = app;
